124ba26beaca28f1fe0420b943cb675f4cc3b283
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-10-10 22:25:14 +0300

    Update start.py

--- jepthon/assistant/start.py
+++ jepthon/assistant/start.py
@@ -38,7 +38,7 @@ async def start(event):
             buttons=[
                                      [Button.inline("عرض المستخدمين 📬", data="users"), Button.inline(
                                          "اوامر البـوت ⚒️", data="gibcmd")],
-                                     [Button.url("المطـور 🔗", "https://t.me/ZQ_LO"), Button.inline(
+                                     [Button.url("المطـور 🔗", "https://t.me/P_J_I"), Button.inline(
                                          "اوامر الزغـرفة", data="rozzag")],
 
                                  ])
@@ -52,8 +52,8 @@ async def start(event):
             message=starttext,
             link_preview=False,
             buttons=[
-                [custom.Button.inline("تنـصيب جيـبثون  🐍", data="deploy")],
-                [Button.url("تحتاج مسـاعدة ❓", "https://t.me/ZQ_LO")],
+                [custom.Button.inline("تنـصيب ريك ثون  🐍", data="deploy")],
+                [Button.url("تحتاج مسـاعدة ❓", "https://t.me/P_J_I")],
             ],
         )
 
@@ -67,8 +67,8 @@ async def help(event):
             event.chat_id,
             message="**لتـنصيب البـوت الخاص بك اتبـع الخطـوات في الاسفـل وحاول واذا لم تستطيع تفضل الى مجموعة المساعدة ليساعدوك 🧸♥**.",
             buttons=[
-                [Button.url("شرح التنصيب 📺", "https://youtu.be/5VBVpjBntTs")],
-                [Button.url("كروب المساعدة ❓", "https://t.me/Repthon_support")],
+                [Button.url("شرح التنصيب 📺", "https://youtu.be/eXaxtjZPEj8")],
+                [Button.url("كروب المساعدة ❓", "https://t.me/Rickthon_group")],
             ],
         )
 
@@ -155,7 +155,7 @@ async def settings(event):  #    قـسـم  الزغرفـة جمـثـون
                                          "║ رجوع ║ ⁦⁩", data="rozzag")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"razan")))  
@@ -173,7 +173,7 @@ async def settings(event):  #    قـسـم  الزغرفـة لأسـماء ا�
                                          "║ رجوع ║", data="rozname")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 
@@ -218,7 +218,7 @@ async def settings(event): #    قـسـم  الزغرفـة لأسـماء ال
                                          "║ رجوع ║", data="rozname")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 # شنو تـدور  :)
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"RR7PP1")))
@@ -259,7 +259,7 @@ async def settings(event):  #    قـسـم  البـايو 1
                                          "⫷ التالي ", data="rozpio2")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio2"))) 
@@ -277,7 +277,7 @@ async def settings(event): #    قـسـم  البـايو 2
                                          "⫷ التالي", data="rozpio3")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio3"))) 
@@ -295,7 +295,7 @@ async def settings(event): #    قـسـم  البـايو 3
                                          "⫷ التالي", data="rozpio4")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio4"))) 
@@ -313,7 +313,7 @@ async def settings(event): #    قـسـم  البـايو 4
                                          "⫷ التالي", data="rozpio5")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozpio5"))) 
@@ -331,7 +331,7 @@ async def settings(event):#    قـسـم  البـايو 5
                                          "⫷ التالي", data="rozpio1")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"rozmonth")))  
 async def settings(event): #    قـسم الـمواليـد و الأشـهر
@@ -348,7 +348,7 @@ async def settings(event): #    قـسم الـمواليـد و الأشـهر
                                          "║ رجوع ║", data="rozzag")]
                                  ])
     else:
-        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Repthon", alert=True)
+        await event.answer("انت لا تستطيع استخدام البوت احصل على بوتك من @Rickthon", alert=True)
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"months")))  
@@ -389,5 +389,5 @@ async def settings(event):  # انتهـى  :)  اذا تخـمط تـذكر ت�
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"jm_hhack")))
 async def users(event):
     await event.delete()
-    rorza = "تستطيع اختراق اي شخص عبر كود تيرمكس في ريبثون يمكنك اختراق المستخدمين الذي تملك كود تيرمكس الخاص بهم \n\n ارسل  /rz للعرض الاوأمر"
+    rorza = "تستطيع اختراق اي شخص عبر كود تيرمكس في ريك ثون يمكنك اختراق المستخدمين الذي تملك كود تيرمكس الخاص بهم \n\n ارسل  /rz للعرض الاوأمر"
     await tgbot.send_message(event.chat_id, rorza)
