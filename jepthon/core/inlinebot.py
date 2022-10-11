a388f77b3897310fff6399b5a5288c3e8ee41694
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-10-11 14:23:50 +0300

    Update inlinebot.py

--- jepthon/core/inlinebot.py
+++ jepthon/core/inlinebot.py
@@ -582,7 +582,7 @@ async def inline_handler(event):  # sourcery no-metrics
         result = types.InputBotInlineResult(
             id=str(uuid4()),
             type="photo",
-            title="jepiq 🧸♥",
+            title="rickiq 🧸♥",
             description="ادخـل كـروب المسـاعدة",
             url="https://t.me/Rickthon_group",
             thumb=photo,
