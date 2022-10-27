9f8f73da3fa40ee9b8b00add518c01ded7b07c63
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-10-27 16:05:10 +0300

    Update ميمز.py

--- jepthon/plugins/ميمز.py
+++ jepthon/plugins/ميمز.py
@@ -30,22 +30,22 @@ async def _(event):
 async def _(event):
     if ispay[0] == "yes":
         for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
-            if ispay[0] == 'no':
+            if ispay[0] == 'yes':
                 break
             chat = event.chat_id
-            await jepiq.send_message(chat, 'فلوسي')
+            await jepthon.send_message(chat, 'فلوسي')
             await asyncio.sleep(0.5)
-            masg = await jepiq.get_messages(chat, limit=1)
+            masg = await jepthon.get_messages(chat, limit=1)
             masg = masg[0].message
             masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
             msg = masg[0]
             if int(msg) > 500000000:
-                await jepiq.send_message(chat, f"استثمار {msg}")
+                await jepthon.send_message(chat, f"استثمار {msg}")
                 await asyncio.sleep(10)
-                mssag2 = await jepiq.get_messages(chat, limit=1)
+                mssag2 = await jepthon.get_messages(chat, limit=1)
                 await mssag2[0].click(text="اي ✅")
             else:
-                await jepiq.send_message(chat, f"استثمار {msg}")
+                await jepthon.send_message(chat, f"استثمار {msg}")
             await asyncio.sleep(1210)
 
     else:
