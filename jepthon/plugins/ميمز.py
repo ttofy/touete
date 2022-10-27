547bafa5faab8b50a189b3471c9147d95875a45f
rick1128 <111245831+rick1128@users.noreply.github.com>
2022-10-27 16:01:43 +0300

    Update ميمز.py

--- jepthon/plugins/ميمز.py
+++ jepthon/plugins/ميمز.py
@@ -26,6 +26,31 @@ async def _(event):
         await event.edit(f"- {response.message.message}\n @RICKTHON")
 
 
+@jepiq.on(events.NewMessage(outgoing=True, pattern=r"\.استثمار وعد (.*)"))
+async def _(event):
+    if ispay[0] == "yes":
+        for i in range(int("".join(event.text.split(maxsplit=2)[2:]).split(" ", 2)[0])):
+            if ispay[0] == 'no':
+                break
+            chat = event.chat_id
+            await jepiq.send_message(chat, 'فلوسي')
+            await asyncio.sleep(0.5)
+            masg = await jepiq.get_messages(chat, limit=1)
+            masg = masg[0].message
+            masg = ("".join(masg.split(maxsplit=2)[2:])).split(" ", 2)
+            msg = masg[0]
+            if int(msg) > 500000000:
+                await jepiq.send_message(chat, f"استثمار {msg}")
+                await asyncio.sleep(10)
+                mssag2 = await jepiq.get_messages(chat, limit=1)
+                await mssag2[0].click(text="اي ✅")
+            else:
+                await jepiq.send_message(chat, f"استثمار {msg}")
+            await asyncio.sleep(1210)
+
+    else:
+        await event.edit("يجب الدفع لاستعمال هذا الامر !")
+#ها
 @jepiq.on(events.NewMessage(outgoing= True,pattern=r'^\.تيك'))
 async def e(event):
                 chat = event.get_chat()
