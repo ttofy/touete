2468df1f0d8e5d51a36513841eb4c25dec04e232
toeteuserbot <117361806+toeteuserbot@users.noreply.github.com>
2022-11-03 18:31:56 +0300

    Update decorators.py

--- jepthon/core/decorators.py
+++ jepthon/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "هذا الخيار ليس لك يجب عليك تنصيب سورس ريك ثون بنفسك  @Rickthon"
+                or "هذا الخيار ليس لك يجب عليك تنصيب سورس تويتي بنفسك  @OOOBO"
             )
             await c_q.answer(
                 HELP_TEXT,
